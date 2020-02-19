c0da9f724196616ddc01d63953e45fb4baa2dc39
rakhmedovrs <rakhmedovrs@gmail.com>
2020-02-19 14:27:01 +0300

    Step 01

---
+++ src/main/java/com/github/rakhmedovrs/SpringBootApp28/SpringBootApp28Application.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedovrs.SpringBootApp28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp28Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rakhmedovrs/SpringBootApp28/SpringBootApp28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedovrs.SpringBootApp28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApp28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
