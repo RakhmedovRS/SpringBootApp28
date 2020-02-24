5d9af95117a0a7cfa1f721a3e89d91a738ad8f78
rassoll <rassoll32@gmail.com>
2020-02-24 20:55:39 +0300

    Section 2 - Step 05

--- src/main/java/com/github/rakhmedovrs/SpringBootApp28/web/controller/LoginController.java
+++ src/main/java/com/github/rakhmedovrs/SpringBootApp28/web/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.github.rakhmedovrs.SpringBootApp28.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Login controller
@@ -13,8 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController
 {
     @RequestMapping("/login")
-    public String loginMessage()
+    public String loginMessage(@RequestParam String name, ModelMap modelMap)
     {
+        modelMap.put("name", name);
         return "login";
     }
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -2,5 +2,5 @@
 <head>
 	<title>Simple JSP page</title>
 </head>
-<body>My first JSP</body>
+<body>My first JSP. Welcome ${name}</body>
 </html>
